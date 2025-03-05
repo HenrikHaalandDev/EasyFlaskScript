78b24d1f510edbd94d4c4971e9def0c2ed63b5f9
HenrikHaalandDev <henrikbibow@icloud.com>
2025-03-05 13:38:27 +0100

    added one line of code | 131

--- setup.bash
+++ setup.bash
@@ -128,6 +128,7 @@ cat <<EOL > ThingsToRemember.md
 # Things to Remember
 ## When pusing to GitHub
 git remote add origin https://github.com/HenrikHaalandDev/"name of repo".git
+git add .
 git commit -m "Adding of needed folders"
 git branch -M main
 git push -u origin main
